33fed5350135cbc5bdfe6e309ec2afc3b76f8de2
Daniel Mills <danielmichaelmills@gmail.com>
2021-11-10 17:05:32 -0600

    Update readSerial
    Works on Windows - needs to work on Linux

--- Pi/readSerial.py
+++ Pi/readSerial.py
@@ -1,5 +1,7 @@
 import serial
 
 with serial.Serial('COM4', 115200, timeout=1) as ser:
-	line = ser.readline()   # read a '\n' terminated line
-	print(line)
+	while(1):
+		line = ser.readline() # read a '\n' terminated line
+		if(line):
+			print(line) # print to terminal
