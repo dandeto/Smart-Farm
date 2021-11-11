7d7c71d6acd4fe7682fe72ebb54f03b560ed188e
Daniel Mills <danielmichaelmills@gmail.com>
2021-11-10 18:48:59 -0600

    Interface with BME280

--- LoRa/LoRa_Recieve/LoRa_Recieve.ino
+++ LoRa/LoRa_Recieve/LoRa_Recieve.ino
@@ -27,15 +27,15 @@ void LoRaData(){
   Heltec.display->clear();
   Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);
   Heltec.display->setFont(ArialMT_Plain_10);
-  Heltec.display->drawString(0, 0, rssi);
   Heltec.display->drawString(0 , 15 , "Received "+ packSize + " bytes");
-  Heltec.display->drawStringMaxWidth(0, 26, 128, packet);
+  Heltec.display->drawStringMaxWidth(0 , 26 , 128, packet);
+  Heltec.display->drawString(0, 0, rssi);  
   Heltec.display->display();
 }
 
 void cbk(int packetSize) {
   packet ="";
-  packetSize = String(packetSize, DEC);
+  packSize = String(packetSize,DEC);
   for (int i = 0; i < packetSize; i++) { packet += (char) LoRa.read(); }
   rssi = "RSSI " + String(LoRa.packetRssi(), DEC) ;
   LoRaData();
