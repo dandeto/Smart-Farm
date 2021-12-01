f414c27e6f4cebf86e792344a6ccea338a790eb7
Stephen Stammen <sms0066@uah.edu>
2021-12-01 17:45:15 -0600

    update parsing - not working yet

--- Pi/readSerial.py
+++ Pi/readSerial.py
@@ -1,7 +1,12 @@
 import serial
+from datetime import datetime as dt
 
-with serial.Serial('/dev/ttyUSB0', 115200, timeout=1) as ser:
-	while(1):
-		line = ser.readline() # read a '\n' terminated line
-		if(line):
-			print(line) # print to terminal
+with open("recieved.txt","a") as file:
+	with serial.Serial('/dev/ttyUSB0', 115200, timeout=1) as ser:
+		while(1):
+			line = ser.readline() # read a '\n' terminated line
+			if(line):
+				line = line[0:-2]
+				print(line) # print to terminal
+				time = dt.now()
+				# file.write(str(time) + "," + str(line))
