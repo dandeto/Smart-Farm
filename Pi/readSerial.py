88934fba0164ef6942db887839713ac639c56ba5
Stephen Stammen <robotcorner88@gmail.com>
2022-01-24 12:18:07 -0600

    completed initial usb listener and parser into csv

--- Pi/readSerial.py
+++ Pi/readSerial.py
@@ -1,12 +1,16 @@
 import serial
 from datetime import datetime as dt
 
-with open("recieved.txt","a") as file:
-	with serial.Serial('/dev/ttyUSB0', 115200, timeout=1) as ser:
-		while(1):
-			line = ser.readline() # read a '\n' terminated line
-			if(line):
-				line = line[0:-2]
-				print(line) # print to terminal
-				time = dt.now()
-				# file.write(str(time) + "," + str(line))
+# Steve PC - Bus 01 - USB 005 and 007 --- /dev/bus/usb/001/005 ---- /dev/ttyUSB5"
+# Change to proper USB port as needed
+serialPort = serial.Serial(port="/dev/ttyUSB1", baudrate=115200, timeout=1, stopbits=serial.STOPBITS_ONE)
+
+while(1):
+    with open("recieved.csv","a") as file:
+        serialInput = serialPort.readline().decode(errors='ignore')
+        if serialInput:
+            data = serialInput.strip()
+            senderId, temp, pressure, altitude, humidity, capacitive = data.split(',')
+            time = dt.now()
+            file.write(f"{str(time)}, {senderId}, {temp}, {pressure}, {altitude}, {humidity}, {capacitive}\n")
+
